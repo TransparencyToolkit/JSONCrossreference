9f9d2676eb09944d4c6813ff785f8b7f0aa69bd2
Shidash <shidash@shidash.com>
2013-11-25 19:50:56 -0800

    Initial code for JSONCrossreference

---
+++ jsoncrossreference.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'jsoncrossreference'
+  s.version     = '0.0.1'
+  s.date        = '2013-11-25'
+  s.summary     = "Crossreferences JSONs"
+  s.description = "Crossreferences JSONs and returns the matches"
+  s.authors     = ["M. C. McGrath"]
+  s.email       = 'shidash@shidash.com'
+  s.files       = ["lib/jsoncrossreference.rb"]
+  s.homepage    =
+    'https://github.com/Shidash/JSONCrossreference'
+  s.license       = 'GPL'
+end
---
+++ lib/jsoncrossreference.rb
@@ -0,0 +1,43 @@
+require 'datacalc'
+
+class JSONCrossreference
+
+  def initialize(input1, input2)
+    @input1 = input1
+    @input2 = input2
+    @output = nil
+  end
+
+  # What: Cross-references fields in two files
+  # Input: Variable number of arrays with name of fields to compare
+  # Output: JSON with matches 
+  # Limitations: Only returns data from one file. Matching multiple fields functions as && not ||. Only takes 2 files input
+  def compare(*fieldnames)
+    if JSON.parse(File.read(@input2)).length < JSON.parse(File.read(@input1)).length
+      file1 = JSON.parse(File.read(@input2))
+      file1length = file1.length
+      file2 = @input1
+
+      tmparray = Array.new
+      fieldnames.each do |field|
+        tmparray.push([field[1], field[0]])
+      end
+      fieldnames = tmparray
+    else 
+      file1 = JSON.parse(File.read(@input1))
+      file1length = file1.length
+      file2 = @input2
+    end
+    
+    tmpfile = Array.new
+    (0..file1length-1).each do |l|
+      fieldnames.each do |field|
+        value = (file1[l])[field[0]]
+        matchnames = [field[1], value]
+        m = DataCalc.new(file2)
+        tmpfile << m.match(matchnames)
+      end
+    end
+    @output = tmpfile.to_json
+  end
+end
